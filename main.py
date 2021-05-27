3b5a0226d4e6d40b301c726dad93f70f30c78e62
Andrew St Clair <andrew_stclair95@hotmail.com>
2021-05-27 18:52:08 +1000

    Get git repo information
    Gets repo information and prints it to /health

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import git
 import time
+import os
+import json
 
 hostName = "localhost"
-serverPort = 8080
+serverPort = 80
 
 class WebServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>Python webserver</title></head><body>", "utf-8"))
-		self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        if self.path == "/health":
+            git = git.Repo('.').git
+            repoHash = git.rev_parse("HEAD", short=True)
+            repoTag = git.describe(tags=True)
+            repoName = os.path.basename(git.rev_parse(show_toplevel=True))
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("<html><head><title>Health</title></head>", "utf-8"))
+            self.wfile.write(bytes("<p>Repo Hash: %s</p>" % repoHash, "utf-8"))
+            self.wfile.write(bytes("<p>Repo Name: %s</p>" % repoName, "utf-8"))
+            self.wfile.write(bytes("<p>Repo Version: %s</p>" % repoTag, "utf-8"))
+        else:
+            self.send_response(404)
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), WebServer)
